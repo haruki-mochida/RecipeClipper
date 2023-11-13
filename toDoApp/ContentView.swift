1683b9b3ce4660f252ae0a9953676538945128e0
haruki-mochida <motimotimotida@gmail.com>
2023-11-13 23:34:53 +0900

    Initial Commit

---
+++ toDoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  toDoApp
+//
+//  Created by 持田晴生 on 2023/11/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ toDoApp/toDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoAppApp.swift
+//  toDoApp
+//
+//  Created by 持田晴生 on 2023/11/13.
+//
+
+import SwiftUI
+
+@main
+struct toDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
