7c1f07a6b3076865e9b50225c661d798e2415ec3
haruki-mochida <motimotimotida@gmail.com>
2023-12-02 19:01:19 +0900

    Todo作成画面(TodoAddViewController)のレイアウト作成

--- toDoApp/TodoAddViewController.swift
+++ toDoApp/TodoAddViewController.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 class TodoAddViewController: UIViewController {
-
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var detailTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        // TextViewのレイアウトをTextField似合わせるためのコード
+        detailTextView.layer.borderWidth = 1.0
+        detailTextView.layer.borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).cgColor
+        detailTextView.layer.cornerRadius = 5.0
+        detailTextView.layer.masksToBounds = true
     }
-    */
+    
+    @IBAction func tapAddButton(_ sender: Any) {
 
+    }
 }
