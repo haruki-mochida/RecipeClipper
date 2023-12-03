33d2a4fcf2c1644b5c18284041c21ee96c8027a1
haruki-mochida <motimotimotida@gmail.com>
2023-12-03 11:08:56 +0900

    Todo編集画面(TodoEditViewController)の追加

--- toDoApp/TodoEditViewController.swift
+++ toDoApp/TodoEditViewController.swift
@@ -8,22 +8,32 @@
 import UIKit
 
 class TodoEditViewController: UIViewController {
-
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var detailTextView: UITextView!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var isDoneLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   override func viewDidLayoutSubviews() {
+        detailTextView.layer.borderWidth = 1.0
+        detailTextView.layer.borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).cgColor
+        detailTextView.layer.cornerRadius = 5.0
+        detailTextView.layer.masksToBounds = true
     }
-    */
+    
+    @IBAction func tapEditButton(_ sender: Any) {
 
+    }
+    @IBAction func tapDoneButton(_ sender: Any) {
+
+    }
+    
+    @IBAction func tapDeleteButton(_ sender: Any) {
+
+    }
 }
+
