52f7661eea1f6035812d32917549409cb66223be
haruki-mochida <motimotimotida@gmail.com>
2023-12-03 12:22:09 +0900

    ログインしているユーザー名の取得

--- toDoApp/TodoListViewController.swift
+++ toDoApp/TodoListViewController.swift
@@ -6,16 +6,28 @@
 //
 
 import UIKit
+import Firebase
 
-class TodoListViewController: UIViewController {
+
+class TodoListViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var userNameLabel: UILabel!
     
-
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // ①ログイン済みかどうか確認
+        if let user = Auth.auth().currentUser {
+            // ②ログインしているユーザー名の取得
+            Firestore.firestore().collection("users").document(user.uid).getDocument(completion: {(snapshot,error) in
+                if let snap = snapshot {
+                    if let data = snap.data() {
+                        self.userNameLabel.text = data["name"] as? String
+                    }
+                } else if let error = error {
+                    print("ユーザー名取得失敗: " + error.localizedDescription)
+                }
+            })
+        }
     }
 
     
