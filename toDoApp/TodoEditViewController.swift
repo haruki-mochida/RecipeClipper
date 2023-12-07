456ac380e0d038624cfab921fb1697e8b0f79de2
haruki-mochida <motimotimotida@gmail.com>
2023-12-07 18:01:44 +0900

    変数userをuserID に、Auth.auth().currentUserをcurrentUser?.uidに

--- toDoApp/TodoEditViewController.swift
+++ toDoApp/TodoEditViewController.swift
@@ -13,19 +13,19 @@ class TodoEditViewController: UIViewController {
     @IBOutlet weak var detailTextView: UITextView!
     @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var isDoneLabel: UILabel!
-    
-    // ①一覧画面から受け取るように変数を用意
+
+// ①一覧画面から受け取るように変数を用意
     var todoId: String!
     var todoTitle: String!
     var todoDetail: String!
     var todoIsDone: Bool!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // ②初期値をセット
+// ②初期値をセット
         titleTextField.text = todoTitle
         detailTextView.text = todoDetail
-        
+
         switch todoIsDone {
         case false:
             isDoneLabel.text = "未完了"
@@ -35,21 +35,21 @@ class TodoEditViewController: UIViewController {
             doneButton.setTitle("未完了にする", for: .normal)
         }
     }
-    
+
    override func viewDidLayoutSubviews() {
-        // レイアウトを設定
+// レイアウトを設定
         detailTextView.layer.borderWidth = 1.0
         detailTextView.layer.borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).cgColor
         detailTextView.layer.cornerRadius = 5.0
         detailTextView.layer.masksToBounds = true
     }
 
-    // ④編集ボタンの実装
+// ④編集ボタンの実装
     @IBAction func tapEditButton(_ sender: Any) {
         if let title = titleTextField.text,
             let detail = detailTextView.text {
-            if let user = Auth.auth().currentUser {
-                Firestore.firestore().collection("users/\(user.uid)/todos").document(todoId).updateData(
+            if let userId = Auth.auth().currentUser?.uid {
+                Firestore.firestore().collection("users/\(userId)/todos").document(todoId).updateData(
                     [
                         "title": title,
                         "detail": detail,
@@ -67,13 +67,13 @@ class TodoEditViewController: UIViewController {
                         }
                 })
             }
-            
+
         }
     }
-    // ③完了、未完了切り替えボタンの実装
+// ③完了、未完了切り替えボタンの実装
     @IBAction func tapDoneButton(_ sender: Any) {
-        if let user = Auth.auth().currentUser {
-            Firestore.firestore().collection("users/\(user.uid)/todos").document(todoId).updateData(
+        if let userId = Auth.auth().currentUser?.uid {
+            Firestore.firestore().collection("users/\(userId)/todos").document(todoId).updateData(
                 [
                     "isDone": !todoIsDone,
             "updatedAt": FieldValue.serverTimestamp()
@@ -92,10 +92,10 @@ class TodoEditViewController: UIViewController {
         }
     }
 
-    // ⑤削除ボタンの実装
+// ⑤削除ボタンの実装
     @IBAction func tapDeleteButton(_ sender: Any) {
-        if let user = Auth.auth().currentUser {
-            Firestore.firestore().collection("users/\(user.uid)/todos").document(todoId).delete(){ error in
+        if let userId = Auth.auth().currentUser?.uid {
+            Firestore.firestore().collection("users/\(userId)/todos").document(todoId).delete(){ error in
                 if let error = error {
                     print("TODO削除失敗: " + error.localizedDescription)
                     let dialog = UIAlertController(title: "TODO削除失敗", message: error.localizedDescription, preferredStyle: .alert)
@@ -109,4 +109,3 @@ class TodoEditViewController: UIViewController {
         }
     }
 }
-
